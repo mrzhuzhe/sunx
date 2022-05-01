259d9d0fea5fda3644c67ca06867ba58e1eef5dc
Zhu Zhe 朱哲 <466648706@qq.com>
2022-05-01 22:34:32 +0800

    first test

--- main.py
+++ main.py
@@ -2,15 +2,23 @@ from scene import Scene
 import taichi as ti
 from taichi.math import *
 
-scene = Scene(exposure=10)
-scene.set_floor(-0.05, (1.0, 1.0, 1.0))
-scene.set_background_color((1.0, 0, 0))
+scene = Scene(exposure=1)
+#scene.set_floor(-0.05, (1.0, 1.0, 1.0))
+#scene.set_background_color((1.0, 0, 0))
 
+scene.set_floor(-0.85, (1.0, 1.0, 1.0))
+scene.set_background_color((0.5, 0.5, 0.4))
+scene.set_directional_light((1, 1, -1), 0.2, (1, 0.8, 0.6))
 
 @ti.kernel
 def initialize_voxels():
     # Your code here! :-)
-    scene.set_voxel(vec3(0, 0, 0), 2, vec3(0.9, 0.1, 0.1))
+    for i in range(10):
+        for j in range(10):
+            for k in range(10):
+                is_light = int(ti.random() > 0.7)
+                if i < 8-k or i > k+1 or j < 8-k or j > k+1:
+                    scene.set_voxel(vec3(i, j, k), 1 + is_light , vec3(0.1, 0.9, 0.1))                
 
 
 initialize_voxels()
