054409c9ec96c9c85066bd9253f3421a4c2347e4
Zhu Zhe 朱哲 <466648706@qq.com>
2022-05-03 00:17:33 +0800

    bowl

--- main.py
+++ main.py
@@ -16,16 +16,25 @@ def initialize_voxels():
     n = 60
     for i, j, k in ti.ndrange((-n, n), (-n, 0), (-n, n)):
         x = ivec3(i, j, k)
-        if  n * n * 0.4 < x.dot(x) < n * n * 0.5:
-            scene.set_voxel(vec3(i, j, k), 1, vec3(0.9, 0.3, 0.3))
+        if  x.dot(x) < n * n * 0.5:
+            if n * n * 0.4 < x.dot(x):
+                scene.set_voxel(vec3(i, j, k), 1, vec3(0.9, 0.3, 0.3))
+            else:
+                if -10<i<10 and -10<j<10: 
+                    scene.set_voxel(vec3(i+15, j-20, -17-j+i), 1 , vec3(0.9, 0.9, 0.1)) 
+                    scene.set_voxel(vec3(i-15, j-20, -15-i), 1 , vec3(0.9, 0.9, 0.1)) 
+                    scene.set_voxel(vec3(i, j-20, -20-j), 1 , vec3(0.9, 0.9, 0.1)) 
 
+    """
+    
     n = 3
     for i, j, k in ti.ndrange((-10*n, 10*n), (-4*n, 4*n), (-4*n, 4*n)):
         is_light = int(ti.random() > 0.7)
-        if k % 2 == 0:
-            scene.set_voxel(vec3(i, j, k*ti.sin(i* 0.2)), 1 + is_light , vec3(0.9, 0.9, 0.1)) 
+        if k%2==0:
+            scene.set_voxel(vec3(i, j, k + 3*ti.sin(i* 0.2)), 1 + is_light , vec3(0.9, 0.9, 0.1)) 
         else:
-            scene.set_voxel(vec3(i, j, k*ti.sin(i* 0.2)), 1 + is_light , vec3(0.1, 0.1, 0.1))  
+            scene.set_voxel(vec3(i, j, k + 3*ti.sin(i* 0.2)), 1 + is_light , vec3(0.1, 0.1, 0.1)) 
+    """
 
 initialize_voxels()
 
