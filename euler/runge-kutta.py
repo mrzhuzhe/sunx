83e79fd5713a4dfdeade6088bd80c1eebaf13a4a
mrzhuzhe <466648706@qq.com>
2022-05-17 20:46:50 +0800

    need mpm

--- euler/runge-kutta.py
+++ euler/runge-kutta.py
@@ -10,13 +10,15 @@ beta = 8.0 / 3.0
 def f(state, t):
     x, y, z = state  # Unpack the state vector
     return sigma * (y - x), x * (rho - z) - y, x * y - beta * z  # Derivatives
+    #return -1 * (x - y)*(1-x-y), x*(2+y), 0
+    #return 4-2*y, 12 - 3*x*x, 0
 
 state0 = np.array([20.0, 10.0, 5.0])
 dt = 0.01
-t = np.arange(0.0, 40.0, dt)
+t = np.arange(0.0, 40, dt)
 
 state = state0
-states2 = odeint(f, state0, t)
+#states2 = odeint(f, state0, t)
 
 states = []
 for i in t:
@@ -31,7 +33,7 @@ states = np.array(states)
 fig = plt.figure()
 ax = fig.gca(projection="3d")
 ax.plot(states[:, 0], states[:, 1], states[:, 2])
-ax.plot(states2[:, 0], states2[:, 1], states2[:, 2])
+#ax.plot(states2[:, 0], states2[:, 1], states2[:, 2])
 
 plt.draw()
 plt.show()
